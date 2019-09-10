ca9d217f64f5548fda4689337f2a64e2ebde4ede
chujun-L <583212965@qq.com>
2019-09-10 18:18:57 +0800

    枚举进程的方法2： 1) EnumProcesses() 2) EnumProcessModules() 3) GetModuleFileNameEx()

--- MFC45/main.cpp
+++ MFC45/main.cpp
@@ -1,5 +1,6 @@
 
 // 工程属性->连接器->输入 添加psapi.lib
+// 有些系统运行时需要提升权限
 
 #include <Windows.h>
 #include <iostream>
@@ -12,6 +13,8 @@ int main()
 	DWORD ProcID[1024];
 	DWORD cbNeeded;
 	DWORD procNum;
+	HMODULE hModes[1024];
+	TCHAR szModName[MAX_PATH];
 
 	if (!EnumProcesses(ProcID, sizeof(ProcID), &cbNeeded)) {
 		cout << "EnumProcesses() failed" << endl;
@@ -21,6 +24,26 @@ int main()
 	// 计算进程的个数
 	procNum = cbNeeded / sizeof(DWORD);
 	cout << "进程的个数: " << procNum  << endl;
+
+	for (DWORD i = 0; i < procNum; ++i) {
+		HANDLE hProc = ::OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+									FALSE, ProcID[i]);
+		if (!hProc) {
+			cout << "进程" << ProcID[i] << " 打开失败" << endl;
+		}
+
+		cout << "进程" << ProcID[i] << " 打开成功" << endl;
+
+		if (::EnumProcessModules(hProc, hModes, sizeof(hModes), &cbNeeded)) {
+			for (DWORD j = 0; j < cbNeeded / sizeof(HMODULE); ++j) {
+				if (::GetModuleFileNameEx(hProc, hModes[j], szModName, 
+										sizeof(szModName) / sizeof(TCHAR))) {
+					cout << j << ":" << szModName << ":" << hModes[j] << endl;
+				}
+			}
+		}
+		CloseHandle(hProc);
+	}
 
 
 	system("pause");
