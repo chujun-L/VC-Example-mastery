b8524f8c427b1fc2b9cd5c6a32a42cbd96a3730e
chujun-L <583212965@qq.com>
2019-09-10 15:38:55 +0800

    添加MFC43工程：该工程有两个目录，分别为应用A、B，利用内存映射进行共享数据

---
+++ MFC43/MFC43A/MFC43A.h
@@ -0,0 +1,32 @@
+﻿
+// MFC43A.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFC43AApp:
+// 有关此类的实现，请参阅 MFC43A.cpp
+//
+
+class CMFC43AApp : public CWinApp
+{
+public:
+	CMFC43AApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFC43AApp theApp;
---
+++ MFC43/MFC43B/MFC43B.h
@@ -0,0 +1,32 @@
+﻿
+// MFC43B.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFC43BApp:
+// 有关此类的实现，请参阅 MFC43B.cpp
+//
+
+class CMFC43BApp : public CWinApp
+{
+public:
+	CMFC43BApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFC43BApp theApp;
