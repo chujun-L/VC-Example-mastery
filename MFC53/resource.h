4eeec246e7a148c70da6b59dd426806823a9c162
Alex <583212965@qq.com>
2019-09-12 09:57:25 +0800

    位图缩放

---
+++ MFC53/resource.h
@@ -0,0 +1,24 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFC53.rc 使用
+//
+#define IDD_MFC53_DIALOG                102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_OPEN_BITMAP                  32774
+#define ID_ZOOMIN_BITMAP                32775
+#define ID_ZOOMOUT_BITMAP               32776
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
