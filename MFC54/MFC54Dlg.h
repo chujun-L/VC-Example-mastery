3c3dd113586504d3b32fe64585b59a05c00262a2
chujun-L <583212965@qq.com>
2019-09-13 09:50:24 +0800

    添加MFC54工程：画刷，画刷的分类有三种：颜色画刷、阴影画刷、图案画刷

---
+++ MFC54/MFC54Dlg.h
@@ -0,0 +1,36 @@
+﻿
+// MFC54Dlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMFC54Dlg 对话框
+class CMFC54Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMFC54Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC54_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	CBrush m_brush;
+	CBitmap m_bMap;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
