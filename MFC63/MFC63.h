a4d75fb33f2368e6d1787c1c234df35c2014e498
chujun-L <583212965@qq.com>
2019-09-16 21:34:33 +0800

    添加MFC63：管理系统(MS)

---
+++ MFC63/MFC63.h
@@ -0,0 +1,32 @@
+﻿
+// MFC63.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFC63App:
+// 有关此类的实现，请参阅 MFC63.cpp
+//
+
+class CMFC63App : public CWinApp
+{
+public:
+	CMFC63App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFC63App theApp;
---
+++ MFC63/MFC63Dlg.h
@@ -0,0 +1,34 @@
+﻿
+// MFC63Dlg.h: 头文件
+//
+
+#pragma once
+
+//#import "C:\Program Files\Common Files\system\ado\msado15.dll" no_namespace rename("EOF", "adoEOF")
+
+// CMFC63Dlg 对话框
+class CMFC63Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMFC63Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC63_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
